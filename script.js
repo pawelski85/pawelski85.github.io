eb87ebd344fe4e0998527fb0a96d8de81706fde8
Pawel <pawel.tomczyk85@gmail.com>
2021-02-16 18:22:28 +0100

    pushBezU

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://tranquil-beyond-46632.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://tranquil-beyond-46632.herokuapp.com/v1/task/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
